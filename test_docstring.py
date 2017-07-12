fdabd1062a2ec3476ffc377b62c22c9c0a621c45
amishpatel-dbe <apatel@ska.ac.za>
2017-07-12 14:35:35 +0200

    Added new .py file to test ast.get_docstring()

---
+++ test_docstring.py
@@ -0,0 +1,25 @@
+'''
+This is a new python file created to test the 'ast' library
+that will be used to extract docstrings from .py files
+'''
+
+#define CONSTANT_VALUE 1
+
+def first_function():
+	'''
+	This is the first function in the test_docstrings.py file.
+	The function has no parameters and will return True.
+	'''
+
+	return True
+
+def second_function(first_parameter):
+	'''
+	This is the second function in the test_docstrings.py file.
+	The function takes in one parameter, expected to be an integer,
+	adds 1 to it and returns the answer.
+	:param first_parameter - integer value
+	:return: result = first_parameter + 1
+	'''
+
+	return first_parameter + 1
